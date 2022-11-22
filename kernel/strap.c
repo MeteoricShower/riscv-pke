dcae33f35e5d04b5863c8e327494e5646ff1fc69
lxy <2321410322@qq.com>
2022-11-22 20:10:35 +0800

    my work on lab2_3 is done.

--- kernel/strap.c
+++ kernel/strap.c
@@ -9,7 +9,8 @@
 #include "pmm.h"
 #include "vmm.h"
 #include "util/functions.h"
-
+#include "util/string.h"
+#include "memlayout.h"
 #include "spike_interface/spike_utils.h"
 
 //
@@ -53,14 +54,25 @@ void handle_mtimer_trap() {
 //
 void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
   sprint("handle_page_fault: %lx\n", stval);
+  uint64 cnt = 1;
   switch (mcause) {
     case CAUSE_STORE_PAGE_FAULT:
       // TODO (lab2_3): implement the operations that solve the page fault to
       // dynamically increase application stack.
       // hint: first allocate a new physical page, and then, maps the new page to the
       // virtual address that causes the page fault.
-      panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
+      
+      // while(stval < USER_STACK_TOP - PGSIZE){
+      //   cnt ++;
+      //   stval += PGSIZE;
+      // }
+      void* new_page = alloc_page();
+      memset(new_page, 0, PGSIZE);
+      // map_pages((pagetable_t)current->pagetable, USER_STACK_TOP - cnt * PGSIZE, PGSIZE, (uint64)new_page, prot_to_type(PROT_WRITE | PROT_READ, 1));
+      map_pages((pagetable_t)current->pagetable, ROUNDDOWN(stval,PGSIZE), PGSIZE, (uint64)new_page, prot_to_type(PROT_WRITE | PROT_READ, 1));
 
+      //panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
+      
       break;
     default:
       sprint("unknown page fault.\n");
